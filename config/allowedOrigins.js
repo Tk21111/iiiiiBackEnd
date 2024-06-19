f548f37ce8eada5a65102b9432269d5afca7d3d1
Tk21111 <101468538+Tk21111@users.noreply.github.com>
2024-06-19 14:54:32 +0700

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,8 @@ const allowedOrigins = [
     'https://www.yoursite.com',
     'http://127.0.0.1:5500',
     'http://localhost:3500',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'https://40fbf2b3-7389-4d1b-b232-bb0b4aebd85b-00-3moev1i4oa4rg.pike.replit.dev'
 ];
 
 module.exports = allowedOrigins;
