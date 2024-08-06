a313972dc81155b2e37f0d7eb87892960c0220ed
Tk21111 <101468538+Tk21111@users.noreply.github.com>
2024-08-06 22:05:03 +0700

    image viewing

--- routes/api/noteApi.js
+++ routes/api/noteApi.js
@@ -10,7 +10,7 @@ const storage = multer.diskStorage({
         cb(null, 'pubilc/image'); // specify the folder where the files should be saved
     },
     filename: function (req, file, cb) {
-        cb(null, Date.now() + path.extname(file.originalname));
+        cb(null, file.originalname);
     }
 });
 
