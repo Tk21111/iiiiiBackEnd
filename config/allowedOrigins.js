b473b6e3214116a898f27c972b3004271aa53544
Tk21111 <101468538+Tk21111@users.noreply.github.com>
2024-10-25 15:33:59 +0700

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-    'https://front-iiiii.onrender.com/',
+    'https://front-iiiii.onrender.com',
     'http://127.0.0.1:5500',
     'http://localhost:3000',
     'https://40fbf2b3-7389-4d1b-b232-bb0b4aebd85b-00-3moev1i4oa4rg.pike.replit.dev'
